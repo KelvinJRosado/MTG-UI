49a33dc4cdad38944e14a01e9be9bef342f08961
KelvinJRosado <kelvinrosado1@yahoo.com>
2025-06-06 17:41:35 -0400

    fix: improve fetchSampleCards function to return detailed card information

--- api/src/mtgsdk-sample.ts
+++ api/src/mtgsdk-sample.ts
@@ -6,10 +6,17 @@ import * as mtg from 'mtgsdk';
  * Returns a Promise resolving to an array of card objects.
  */
 export async function fetchSampleCards() {
-  // Fetch 5 cards with the name 'Black Lotus' as a sample
+  // Fetch cards with the name 'Black Lotus' as a sample
   return new Promise((resolve, reject) => {
     mtg.card.where({ name: 'Black Lotus' })
-      .then(cards => resolve(cards))
+      .then(cards => {
+        if (cards && cards.length > 0) {
+          const card = cards[0];
+          resolve({ name: card.name, text: card.text });
+        } else {
+          resolve(null);
+        }
+      })
       .catch(err => reject(err));
   });
 }
